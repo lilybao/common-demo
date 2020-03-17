242945b21b1aa6804e0caaf7e119df2aa10ca69f
lilybao <752415416@qq.com>
2020-03-17 18:52:28 +0800

    测试锁

--- multithread-demo/src/main/java/com/baoli/thread/lock/LockDemo.java
+++ multithread-demo/src/main/java/com/baoli/thread/lock/LockDemo.java
@@ -1,5 +1,7 @@
 package com.baoli.thread.lock;
 
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
 import java.util.concurrent.locks.Lock;
 import java.util.concurrent.locks.ReentrantLock;
 
@@ -13,24 +15,64 @@ public class LockDemo {
     public static void main(String[] args) {
 //        Lock lock = new Lock();
 //        lock.lock();
-        ReentrantLock reentrantLock = new ReentrantLock();
+//        ReentrantLock reentrantLock = new ReentrantLock();
 //        reentrantLock.lockInterruptibly();
-        test();
+//        test();
+        ExecutorService executorService = Executors.newFixedThreadPool(4);
+        Test test = new Test();
+        MyThread thread1 = new MyThread(test);
+        MyThread thread2 = new MyThread(test);
+        thread1.start();
+        thread2.start();
+
+        try {
+            Thread.sleep(2000);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+        thread2.interrupt();
     }
 
     private synchronized static void test() {
 
     }
 
-    class Test{
-        public void insert(Thread thread){
-            System.out.println(thread.getName()+"获取了锁");
+    static class Test {
+        private Lock lock = new ReentrantLock();
+
+        public void insert(Thread thread) throws InterruptedException {
+            lock.lockInterruptibly();  //响应中断的使用方法
+            System.out.println(thread.getName() + "获取了锁");
+            long l = System.currentTimeMillis();
+            try {
+                while (true) {
+                    if (System.currentTimeMillis() - l > Integer.MAX_VALUE) {
+                        break;
+                    }
+                }
+            } finally {
+                System.out.println(Thread.currentThread().getName() + "执行了finally");
+                lock.unlock();
+                System.out.println(Thread.currentThread().getName() + "释放了锁");
+            }
         }
     }
-    class MyThread extends  Thread{
+
+   static class MyThread extends Thread {
+        Test test;
+
+        public MyThread(Test test) {
+            this.test = test;
+        }
+
         @Override
         public void run() {
-            System.out.println();
+            try {
+                test.insert(Thread.currentThread());
+            } catch (InterruptedException e) {
+                System.out.println(Thread.currentThread().getName() + "被中断");
+            }
+
         }
     }
 }
