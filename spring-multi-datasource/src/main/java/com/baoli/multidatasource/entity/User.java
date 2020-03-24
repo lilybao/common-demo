cb9544f45f1fda86fed24316d3c467a35209239a
lilybao <752415416@qq.com>
2020-03-24 19:05:43 +0800

    测试用户

--- spring-multi-datasource/src/main/java/com/baoli/multidatasource/entity/User.java
+++ spring-multi-datasource/src/main/java/com/baoli/multidatasource/entity/User.java
@@ -1,11 +1,15 @@
 package com.baoli.multidatasource.entity;
 
+import lombok.Data;
+
 /**
  * @program: common-demo
  * @description: 用户
  * @author: li baojian
  * @create: 2020-03-23 18:40
  */
-
+@Data
 public class User {
+    private String name;
+    private String age;
 }
